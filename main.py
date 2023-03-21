c034d8af30011fae5bcb25ed70ff750406f0b4fb
TanTheMan09 <127160237+TanTheMan09@users.noreply.github.com>
2023-03-21 18:53:35 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+def on_button_pressed_ab():
+    global Hard_Questions
+    Hard_Questions = ["Qu'est-ce que le principe de Pascal?",
+        "Citez les 6 règles de la théorie des particules",
+        "Quelle est la densité du fer?"]
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+choice_2 = 0
 choice = 0
-questions = ["What is a fluid?",
-    "What is viscosity?",
-    "What is the density of pure gold?",
-    "True or False D=M/V",
-    "What is more viscous oil or water?",
-    "What is the density of water?"]
+Hard_Questions: List[str] = []
+questions = ["Qu'est-ce qu'un fluide?",
+    "Qu'est-ce que c'est la viscosité?",
+    "Quelle est la densité de l'or pur?",
+    "Vrai ou faux D=M/V",
+    "Qu'est-ce qui est le plus visqueux : l'huile ou l'eau?",
+    "Quelle est la densité de l'eau?"]
 
 def on_forever():
     global choice
@@ -14,8 +23,15 @@ def on_forever():
 basic.forever(on_forever)
 
 def on_forever2():
+    global choice_2
+    if input.button_is_pressed(Button.AB):
+        choice_2 = randint(0, 2)
+        basic.show_string("" + (Hard_Questions[choice_2]))
+basic.forever(on_forever2)
+
+def on_forever3():
     global choice
     if input.button_is_pressed(Button.A):
         choice = randint(0, 5)
         basic.show_string("" + (questions[choice]))
-basic.forever(on_forever2)
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
+input.onButtonPressed(Button.AB, function () {
+    Hard_Questions = ["Qu'est-ce que le principe de Pascal?", "Citez les 6 règles de la théorie des particules", "Quelle est la densité du fer?"]
+})
+let choice_2 = 0
 let choice = 0
+let Hard_Questions: string[] = []
 let questions = [
 "Qu'est-ce qu'un fluide?",
-"Qu'est-ce que la viscosité?",
+"Qu'est-ce que c'est la viscosité?",
 "Quelle est la densité de l'or pur?",
 "Vrai ou faux D=M/V",
 "Qu'est-ce qui est le plus visqueux : l'huile ou l'eau?",
@@ -11,6 +16,12 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
         led.stopAnimation()
         choice = randint(0, 5)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        choice_2 = randint(0, 2)
+        basic.showString("" + (Hard_Questions[choice_2]))
     }
 })
 basic.forever(function () {
